1203039ef8db8fad2205e9753c2a0e1b8078cf05
MoeHtetb <moehtetoo@MoeHtetbs-MacBook-Air.local>
2024-12-27 07:33:19 +0630

    first state

---
+++ learn-js/test.js
@@ -0,0 +1,45 @@
+// let result = [];
+// let resultIndex = 0;
+
+// const area = (width, high) => {
+//   const answer = width * high;
+//   result[resultIndex++] = { width, high, area: answer };
+//   console.log(`${answer}sqft`);
+// };
+
+// area(23, 32);
+// area(22, 34);
+// console.table(result);
+
+// const exchangeMmk = (amount, currency) => {
+//   const currencyRate = rate[currency];
+//   console.log(`${currency} is ${currencyRate}`);
+//   return amount * currencyRate;
+// };
+
+// console.log(exchangeMmk(23, "usd"));
+// console.log(exchangeMmk(23, "eur"));
+// console.log(exchangeMmk(23, "sg"));
+
+// const rate = {
+//   usd: 2100,
+//   eur: 2333,
+//   sg: 2231,
+// };
+// const exchangeFromMmkToCurrency = (amount, currency) => {
+//   const currencyRate = rate[currency];
+//   const result = amount / currencyRate;
+
+//   return `${result.toFixed(1)} ${currency}`;
+// };
+
+// console.log(exchangeFromMmkToCurrency(50000, "usd"));
+// console.log(exchangeFromMmkToCurrency(50000, "eur"));
+// console.log(exchangeFromMmkToCurrency(50000, "sg"));
+
+// console.log(true ? "this is true " : "false");
+// console.log(!!!!true);
+
+//count
+
+let count = 0;
